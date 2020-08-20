17db17cab3b0ca97201276d30a25115590e478b5
Ophelie Roux <ophelie.roux@gmail.com>
2020-08-20 12:43:23 +0200

    added some photos to the seed and descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,12 @@ Product.destroy_all
 User.destroy_all
 
 puts 'create some products'
-a = Product.create!(name: 'yoyo stroller',description: 'black yoyo black yoyo black yoyo black yoyo black yoyo black yoyo', price: '5', city: 'Bordeaux')
-Product.create!(name: 'pink yoyo stroller',description: 'pink yoyo pink yoyo pink yoyo pink yoyo pink yoyo pink yoyo', price: '4', city: 'Bordeaux')
-Product.create!(name: 'crib',description: 'baby bedside bassinet, 3 in 1 portable beside sleeper crib', price: '3', city: 'Bordeaux')
-Product.create!(name: 'double toddlerbed',description: 'delta children interactive wood toddlerbed disney minnie mouse', price: '5', city: 'Nantes')
-Product.create!(name: 'single toddlerbed',description: 'delta children 3D footboard toddlerbed, cars 3 twingle stars fiber', price: '6', city: 'Niort')
+a = Product.create!(name: 'Yoyo Stroller',description: 'YOYO2 stroller folds and unfolds with one hand, can be carried over the shoulder, slips in anywhere, stores easily and travels as carry-on-baggage', price: '5', city: 'Bordeaux', photo:'yoyostroller.jpg')
+Product.create!(name: 'Bugaboo stroller',description: 'Compact without compromise! The streamlined design of the CRUZ V2 gives you more without weighing you down. So stroll down bumpy streets.', price: '4', city: 'Bordeaux', photo: 'bugaboostroller.jpg')
+Product.create!(name: 'Crib',description: 'Baby bedside bassinet, 3 in 1 portable beside sleeper crib', price: '3', city: 'Bordeaux', photo: 'crib.jpg')
+Product.create!(name: 'Housebed',description: 'Delta children interactive wood toddlerbed disney minnie mouse', price: '5', city: 'Nantes', photo: 'housebed.jpg' )
+Product.create!(name: 'Toddlerbed',description: 'The Ella Cot Bed adapts effortlessly from cot to bed providing the perfect sleeping environment for your little one. To keep up with your growing child.', price: '6', city: 'Niort', photo: 'childbed.jpg')
+Product.create!(name: 'Baby carrier',description: 'During those first months, you hold your new-born in your arms almost all the time. You feed, lull to sleep, comfort and cuddle them around the clock.', price: '6', city: 'Niort', photo: 'mumbabycarrier.jpg')
 
 puts 'create some users'
 b = User.create!(first_name:"ophelie", last_name:"roux", email:"ophelie@gmail.com", password:"ophelie")
