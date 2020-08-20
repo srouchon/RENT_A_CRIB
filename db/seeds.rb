312ae175e141a686897f333b2eac65a3beb91fb8
Ophelie Roux <ophelie.roux@gmail.com>
2020-08-20 14:25:30 +0200

    finished seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,19 @@ Booking.destroy_all
 
 puts 'create some products'
 a = Product.create!(name: 'Yoyo Stroller',description: 'YOYO2 stroller folds and unfolds with one hand, can be carried over the shoulder, slips in anywhere, stores easily and travels as carry-on-baggage', price: '5', city: 'Bordeaux', photo:'yoyostroller.jpg')
-Product.create!(name: 'Bugaboo stroller',description: 'Compact without compromise! The streamlined design of the CRUZ V2 gives you more without weighing you down. So stroll down bumpy streets.', price: '4', city: 'Bordeaux', photo: 'bugaboostroller.jpg')
+Product.create!(name: 'Bugaboo Stroller',description: 'Compact without compromise! The streamlined design of the CRUZ V2 gives you more without weighing you down. So stroll down bumpy streets.', price: '4', city: 'Bordeaux', photo: 'bugaboostroller.jpg')
 Product.create!(name: 'Crib',description: 'Baby bedside bassinet, 3 in 1 portable beside sleeper crib', price: '3', city: 'Bordeaux', photo: 'crib.jpg')
-Product.create!(name: 'Housebed',description: 'Delta children interactive wood toddlerbed disney minnie mouse', price: '5', city: 'Nantes', photo: 'housebed.jpg' )
-Product.create!(name: 'Toddlerbed',description: 'The Ella Cot Bed adapts effortlessly from cot to bed providing the perfect sleeping environment for your little one. To keep up with your growing child.', price: '6', city: 'Niort', photo: 'childbed.jpg')
+Product.create!(name: 'House Bed',description: 'Delta children interactive wood toddlerbed disney minnie mouse', price: '5', city: 'Nantes', photo: 'housebed.jpg' )
+Product.create!(name: 'Toddler Bed',description: 'The Ella Cot Bed adapts effortlessly from cot to bed providing the perfect sleeping environment for your little one. To keep up with your growing child.', price: '6', city: 'Niort', photo: 'childbed.jpg')
 Product.create!(name: 'Baby carrier',description: 'During those first months, you hold your new-born in your arms almost all the time. You feed, lull to sleep, comfort and cuddle them around the clock.', price: '6', city: 'Niort', photo: 'mumbabycarrier.jpg')
+Product.create!(name: 'Brown Bed',description: 'Lovely bed for your baby. Comfortable mattress and lovely style for a cute baby room.', price: '6', city: 'Niort', photo: 'brownbed.jpg')
+Product.create!(name: 'Chair',description: 'Great chair with a height that fits most tables. Your toddler will be able to share family meals with the rest of the family.', price: '6', city: 'Niort', photo: 'chair.jpg')
+Product.create!(name: 'Brown Crib',description: 'Great crib for your baby. Lovely sense of nesting for long nights.', price: '6', city: 'Niort', photo: 'browncrib.jpg')
+Product.create!(name: 'Lego',description: 'Well-known game that will keep your child busy and happy for hours.', price: '1', city: 'Paris', photo: 'lego.jpg')
+Product.create!(name: 'Mario',description: 'A few figurines that your child will love to play with at any given time.', price: '3', city: 'Bayonne', photo: 'mario.jpg')
+Product.create!(name: 'Outdoor Chair',description: 'A great chair to have a pic nic on the go or in your garden. Very safe and light. Lovely to travel during the summer.', price: '2', city: 'Bayonne', photo: 'outdoorchair.jpg')
+Product.create!(name: 'White Bed',description: 'Lovely bed for your lovely baby. Enjoy this stylish bed.', price: '6', city: 'Bayonne', photo: 'whitebed.jpg')
+Product.create!(name: 'Yellow Duck',description: 'A lovely and classical toy that any child will enjoy in his bath. From an early age, children will enjoy watching them float.', price: '1', city: 'St Denis de la Réunion', photo: 'yellowduck.jpg')
 
 puts 'create some users'
 b = User.create!(first_name:"ophelie", last_name:"roux", email:"ophelie@gmail.com", password:"ophelie")
